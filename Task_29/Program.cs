269f63ff2a736c54b9b0db3daaf6e1ced44917cc
Denis <denis_malikov_01@mail.ru>
2023-04-21 23:12:42 +0500

    Решил задачу 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+double [] CreatingAnArray()
+{
+    int len = 0;
+    System.Console.WriteLine("Введите длину массива (целое число)");
+    len = Convert.ToInt32(Console.ReadLine());
+    if(len > 0)
+    {
+        double [] massive = new double [len]; 
+        for (int i = 0; i < len; i++)
+        {
+            System.Console.WriteLine("Введите элемент массива");
+            massive [i]  = double.Parse(Console.ReadLine()!);
+        }
+        return massive;
+    }
+    else
+    {
+        System.Console.WriteLine("Ошибка! Вы ввели отрицательное число ");
+        double [] errors = new double [0];
+        return errors;
+    }
+}
+
+
+void PrintArray(double [] array)
+{
+    System.Console.Write("{");
+    System.Console.Write(String.Join(", ",array));
+    System.Console.Write("}");
+}
+
+double [] massive = CreatingAnArray();
+PrintArray(massive);
